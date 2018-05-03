5717dd096384b20671336c525bcccd576ed9db35
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-05-02 19:13:55 -0700

    add image table to store images you want to add to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_27_214743) do
+ActiveRecord::Schema.define(version: 2018_05_03_020750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
